c05bfddc62b795ce8cd2dad5dde9a9568c70f3d5
Baisa54 <totitobaez1011@gmail.com>
2024-11-18 15:09:09 -0300

    PrimerosPasos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import ar.edu.unlu.molino195157.Ficha;
+import ar.edu.unlu.molino195157.Jugador;
+import ar.edu.unlu.molino195157.Posicion;
+import ar.edu.unlu.molino195157.Tablero;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Jugador jugador = new Jugador();
+        Ficha ficha = new Ficha(jugador);
+        Tablero tablero = new Tablero();
+        tablero.ocuparPosicion("A1", ficha);
+        tablero.mostrarPosiciones();
     }
 }
